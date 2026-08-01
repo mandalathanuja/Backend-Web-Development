465bbe203e07ac8a8dc73e7f89878ec45cddd964
Thanuja <mthanuja44@gmail.com>
2026-08-01 10:03:26 +0530

    Complete Supporting Layers assignment

---
+++ Module 2/Supporting Layers/utils/validateRequest.js
@@ -0,0 +1,16 @@
+const { validationResult } = require('express-validator');
+
+function validateRequest(req, res, next) {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(422).json({
+      error: 'Validation failed',
+      details: errors.array()
+    });
+  }
+
+  next();
+}
+
+module.exports = validateRequest;
